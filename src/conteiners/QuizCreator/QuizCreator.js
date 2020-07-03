9f851bef4153a899c63fb6161a073fc9f160e561
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-07-03 12:36:00 +0200

    implement form handlers

--- src/conteiners/QuizCreator/QuizCreator.js
+++ src/conteiners/QuizCreator/QuizCreator.js
@@ -45,10 +45,36 @@ export default class QuizCreator extends Component {
 
     addQuestionHandler = event => {
         event.preventDefault();
+        const quiz = this.state.quiz.concat();
+        const index = quiz.length + 1;
+
+        const {question, option1, option2, option3, option4} = this.state.formControls;
+
+        const questionItem = {
+            question: question.value,
+            id: index,
+            rightAnswerId: this.state.rightAnswerId,
+            answers: [
+                {text: option1.value, id: option1.id},
+                {text: option2.value, id: option2.id},
+                {text: option3.value, id: option3.id},
+                {text: option4.value, id: option4.id}
+            ]
+        }
+
+        quiz.push(questionItem);
+
+        this.setState({
+            quiz: quiz,
+            isFormValid: false,
+            rightAnswerId: 1,
+            formControls: createFormControls()
+        })
     }
 
-    createQuizHandler = () => {
-
+    createQuizHandler = event => {
+        event.preventDefault();
+        console.log(this.state.quiz)
     }
 
     changeHandler = (value, controlName) => {
