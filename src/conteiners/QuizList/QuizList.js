e39e6da75f9f15ab0d9e5795cb86b1e7d1a6658f
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-07-03 12:46:04 +0200

    add axios

--- src/conteiners/QuizList/QuizList.js
+++ src/conteiners/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 import './QuizList.css';
+import axios from 'axios';
 
 export default class QuizList extends Component {
 
@@ -15,6 +16,14 @@ export default class QuizList extends Component {
             )
         })
     };
+
+    componentDidMount() {
+        axios.get('https://react-quiz-f934d.firebaseio.com/quiz.json')
+            .then( response =>
+                console.log(response)
+
+            )
+    }
 
     render() {
         return (
